e33b6c49c1af1baf9078d64614a54d53d0905fc5
Anastasiya <abdeveloperit@gmail.com>
2023-08-21 22:24:31 +0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,38 @@
-const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
-  entry: ['./src/index.js', './src/index.scss'],
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    clean: true,
   },
   module: {
     rules: [
       {
-        test: /.s?css$/,
+        test: /\.s?css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
-        test: /.(jpg|png)$/,
-        type: 'asset/resource',
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 8192,
-            fileName: '[name].[ext]',
-            outputPath: 'images',
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              outputPath: 'images',
+              limit: 8192,
+            },
           },
-        },
-      },
-      {
-        test: /.js$/,
-        use: 'babel-loader',
+        ],
       },
     ],
   },
   plugins: [
-    new webpack.ProgressPlugin(),
-    new htmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
-    new CleanWebpackPlugin(),
   ],
 };
